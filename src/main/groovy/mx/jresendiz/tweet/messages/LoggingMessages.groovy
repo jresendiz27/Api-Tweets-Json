ea354466d7035c76f5c4f488e957c5a2d930f2f2
Juan Resendiz <jresendiz27@gmail.com>
2018-02-18 02:14:41 -0600

    Creating LoggingMessages class, for handling general logging messages

---
+++ src/main/groovy/mx/jresendiz/tweet/messages/LoggingMessages.groovy
@@ -0,0 +1,9 @@
+package mx.jresendiz.tweet.messages
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class LoggingMessages {
+    final static String VERTICLE_DEPLOYED = "Verticle @VerticleId has been deployed ..."
+    final static String VERTICLE_UNDEPLOYED = "Verticle @VerticleId has been undeployed ..."
+}
